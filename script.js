5b0f1559099f5a6b849ef643a9c41ad1554a9480
moiseszip <moisesasdm226@gmail.com>
2024-06-20 11:06:54 -0300

    apply css changes and js counter

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded",() => {
+    
+    const dropDate = new Date('2024-07-08T09:00:00');
+
+    function updateCounter(){
+
+        const now = new Date;
+        const timeDifference = dropDate - now;
+        // return in ms
+
+        // convert ms in days, hours, mins and secs + math.floor to round 
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+        document.getElementById('counter').innerHTML = `drop in ${days}d:${hours}h:${minutes}m:${seconds}s`;
+    }
+
+    setInterval(updateCounter, 1000);
+
+    updateCounter();
+});
